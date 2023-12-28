2d783ea3d4acc7f95999a48883339e7c89bc1b68
Sergio0831 <sergejs.ivcenko@gmail.com>
2023-12-28 17:16:54 +0000

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      injectRegister: 'auto',
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
